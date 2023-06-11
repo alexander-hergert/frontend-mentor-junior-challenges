3142a13a372fad1078190e8d33f4b788da0be7df
Alexander Hergert <alexander.hergert1989@gmail.com>
2023-06-11 13:00:27 +0200

    Change background color to another white. Add navbar effect.

--- sunnyside-agency-landing-page-main/index.js
+++ sunnyside-agency-landing-page-main/index.js
@@ -1,35 +1,45 @@
 // Toggle function
 const button = document.querySelector(".burger-button");
-const navbar = document.querySelector("#navbar");
+const navbarContainer = document.querySelector("#navbar");
+const nav = document.querySelector("nav");
 let toggle = false;
 const windowSize = 870;
 
 const handleToggle = (e) => {
   e.preventDefault();
   if (toggle) {
-    navbar.classList.remove("toggle");
+    navbarContainer.classList.remove("toggle");
     toggle = false;
   } else {
-    navbar.classList.add("toggle");
+    navbarContainer.classList.add("toggle");
     toggle = true;
   }
 };
 
-window.addEventListener("resize", function () {
+window.addEventListener("resize", () => {
   if (toggle && window.innerWidth > windowSize) {
-    navbar.classList.remove("toggle");
+    navbarContainer.classList.remove("toggle");
     toggle = false;
   }
 });
 
 button.addEventListener("click", handleToggle);
 
-//Smooth scrolling
+//Navbar scroll effect
+window.onscroll = () => {
+  const scrollDistance = 100;
+  if (window.pageYOffset > scrollDistance) {
+    nav.classList.add("nav-scroll");
+  } else if (window.pageYOffset < scrollDistance) {
+    nav.classList.remove("nav-scroll");
+  }
+};
 
-document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-  anchor.addEventListener("click", function (e) {
+//Smooth scrolling
+document.querySelectorAll('a[href^="#"]').forEach((link) => {
+  link.addEventListener("click", (e) => {
     e.preventDefault();
-    document.querySelector(anchor.getAttribute("href")).scrollIntoView({
+    document.querySelector(link.getAttribute("href")).scrollIntoView({
       behavior: "smooth",
     });
   });
